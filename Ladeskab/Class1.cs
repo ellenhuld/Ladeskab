ed57dbb385591b76202471a96f5e946702a873af
Ellen Huld Thordardottir <au582001@uni.au.dk>
2020-02-26 15:27:29 +0100

    Test Jenkins

--- Ladeskab/Class1.cs
+++ Ladeskab/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ladeskab
 {
-    public class Class1
+    public class TestLadeskab
     {
     }
 }
--- TestUnitLadeskab/TestLadeskab.cs
+++ TestUnitLadeskab/TestLadeskab.cs
@@ -2,7 +2,7 @@
 
 namespace TestUnitLadeskab
 {
-    public class Class1
+    public class TestLadeskab
     {
     }
 }

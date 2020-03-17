ecd3b6f5d4ea56da79518bdb84c39947ca5a7bac
Ellen Huld Thordardottir <au582001@uni.au.dk>
2020-03-17 22:46:33 +0100

    ICharge og USBCharger plus station control setup

--- Ladeskab/Interface/ICharge.cs
+++ Ladeskab/Interface/ICharge.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Ladeskab.Interface
 {
-    class ICharge
+    public class CurrentEventArgs : EventArgs
     {
+        // Value in mA (milliAmpere)
+        public double Current { set; get; }
     }
+
+    public interface ICharge
+    {
+        // Event triggered on new current value
+        event EventHandler<CurrentEventArgs> CurrentValueEvent;
+
+        // Direct access to the current current value
+        double CurrentValue { get; }
+
+        // Require connection status of the phone
+        bool Connected { get; }
+
+        // Start charging
+        void StartCharge();
+        // Stop charging
+        void StopCharge();
+    }
+
 }

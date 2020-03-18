1f950214d79873d5ef86f6827ab2f27315501fdb
Rune Matthias Hell <au550573@uni.au.dk>
2020-03-18 01:28:59 +0100

    RFIDReader og IReader

--- Ladeskab/RFIDReader.cs
+++ Ladeskab/RFIDReader.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ladeskab.Interface;
 
 namespace Ladeskab
 {
-    class RFIDReader
+    class RFIDReader : IReader
     {
+        private int _oldid;
+        public event EventHandler<ReadtagChangedEventArgs> TagDataEvent;
+        
+        public void Readtag(int id)
+        {
+            ReadtagChanged(new ReadtagChangedEventArgs { tag = id });
+            _oldid = id;
+        }
+        protected virtual void ReadtagChanged(ReadtagChangedEventArgs e)
+        {
+            TagDataEvent?.Invoke(this, e);
+        }
     }
 }

98943359c3f09560b7159b9b200b9fa988d70709
Ellen Huld Thordardottir <au582001@uni.au.dk>
2020-03-18 18:16:00 +0100

    TestSetup

--- TestUnitLadeskab/TestLadeskab.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TestUnitLadeskab
-{
-    public class TestLadeskab
-    {
-    }
-}
---
+++ TestUnitLadeskab/TestLedeskab.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interface;
+using Ladeskab;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TestUnitLadeskab
+{
+    [TestFixture]
+    public class TestLedeskab
+    {
+        private  USBCharger _uut;
+        private ICharge _charge;
+
+            [SetUp]
+        public void Setup()
+        {
+            _uut = new USBCharger();
+            _charge = Substitute.For<ICharge>();
+        }
+
+        [Test]
+        public void CurrentIsConnected()
+        {
+
+        }
+        
+    }
+}

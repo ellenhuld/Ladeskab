862017ddb6b19747b94aff83369c4f34d7517ab8
Ellen Huld Thordardottir <au582001@uni.au.dk>
2020-03-24 14:34:43 +0100

    Test for: ConsolWrite, FileWriter og TimeProvider Samt beskrivelse af FileWriter

---
+++ TestUnitLadeskab/TestUnitConsoleWrite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab;
+using Ladeskab.Interface;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TestUnitLadeskab
+{
+    [TestFixture]
+    public class TestUnitConsoleWrite
+    {
+        private ConsoleWrite _uut;
+        private IConsoleWrite iconsoleWrite;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            //iconsoleWrite = Substitute.For<IConsoleWrite>();
+            _uut = new ConsoleWrite();
+
+        }
+
+        [Test]
+
+        public void WriteLine_ConsoleWrite_Input()
+        {
+            string message = "Console Write Test"; //bruges til test input
+            _uut.WriteLine(message); //input (det er det her som er console write)
+            Assert.That(message, Is.EqualTo(message));
+
+        }
+    }
+}
---
+++ TestUnitLadeskab/TestUnitFileWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab;
+using Ladeskab.Interface;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TestUnitLadeskab
+{
+    [TestFixture]
+    public class TestUnitFileWriter
+    {
+        private FileWriter _uut;
+        private IFileWriter ifileWriter;
+        private string _fileWriter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //ifileWriter = Substitute.For<IFileWriter>();
+            //_uut = new FileWriter(_fileWriter);
+
+            _uut = new FileWriter("LogFile.txt");
+
+        }
+
+        [Test]
+        public void Test_WriteLine_InFile()
+        {
+            _uut.WriteLine("Writing in file"); //Arrance (input)
+            Console.WriteLine($"System.IO.Directory.GetCurrentDirectory()"); //Act
+            Assert.That("LogFile.txt", Does.Exist); //Assert
+        }
+
+    }
+}
